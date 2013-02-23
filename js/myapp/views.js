dc582f4112b43239f8f7757f0707a449c3a5ecce
Martin Fochler <mafo.work@gmail.com>
2013-02-23 16:13:15 +0100

    templating in index.html

--- js/myapp/views.js
+++ js/myapp/views.js
@@ -6,41 +6,63 @@
  * To change this template use File | Settings | File Templates.
  */
 
-PersonView = Backbone.View.extend({
+(function (){
 
-    initialize:function(){
+PersonView = Backbone.View.extend({
+	initialize: function() {
+		console.log("init person view");
+		this.render();
+	},
+
+    render:function(){
 		// Compile the template using underscore
         var template = _.template( $("#person_template").html(), {} );
         // Load the compiled HTML into the Backbone "el"
         this.$el.html( template );
+        return this;
     }
 
 });
 
 AddressView = Backbone.View.extend({
+	initialize: function() {
+		console.log("init address view");
+		this.render();
+	},
 
-    initialize:function(){
+    render:function(){
 		// Compile the template using underscore
         var template = _.template( $("#address_template").html(), {} );
         // Load the compiled HTML into the Backbone "el"
         this.$el.html( template );
+        return this;
     }
 
 });
 
 SummaryView = Backbone.View.extend({
+	initialize: function() {
+		console.log("init summary view");
+		this.render();
+	},
 
-    initialize:function(){
+    render:function(){
 		// Compile the template using underscore
         var template = _.template( $("#summary_template").html(), {} );
         // Load the compiled HTML into the Backbone "el"
         this.$el.html( template );
+        return this;
     }
 
 });
 
 // creating the views
 
-var person_view = new PersonView({ el: $("#container") });
-var address_view = new AddressView({ el: $("#container") });
-var summary_view = new SummaryView({ el: $("#container") });
+
+document.ready = function () {
+	var person_view = new PersonView({ el: $("#container") });
+}
+//var address_view = new AddressView({ el: $("#container") });
+//var summary_view = new SummaryView({ el: $("#container") });
+
+}());
